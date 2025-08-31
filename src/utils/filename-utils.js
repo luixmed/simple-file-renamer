024005e471f2e803b2d6d4f97636b16cf031c42b
Luis Medina <i202215560@cibertec.edu.pe>
2025-08-31 11:26:39 -0500

    refactor(rename): externalize filename prefix via config object
    - Replaced hardcoded "new_" prefix with dynamic value from config.prefix
    - Improved flexibility and reusability of createNewFilesNames utility
    - Supports centralized control of naming conventions across workflows

--- src/utils/filename-utils.js
+++ src/utils/filename-utils.js
@@ -1,7 +1,11 @@
+import { config } from "./config.js";
+
 export function createNewFilesNames(text) {
   return text.split("\n").reduce((namePairs, oldName) => {
     oldName = oldName.trim();
-    if (oldName) namePairs.push([oldName, `new_${oldName}`]);
+    if (oldName) {
+      namePairs.push([oldName, `${config.prefix}${oldName}`]);
+    }
     return namePairs;
   }, []);
 }
